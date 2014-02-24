68c50ac93a667a31fe074bb97e6d21ba6dd314cd
Matt Thomas <matt@platformprojects.org>
2014-02-24 13:47:37 +0000

    Finally pushed past mental block and made progress

---
+++ excersise_8.rb
@@ -0,0 +1,36 @@
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	students = []
+	name = gets.chomp
+	while !name.empty? do
+		puts "Enter Cohort"
+		cohort = gets.chomp
+		cohort = "N/A" if cohort.empty?
+		students << {:name => name, :cohort => cohort}
+		puts "Now we have #{students.length} students"
+		name = gets.chomp
+	end
+	students
+end
+
+def print_header
+	puts "The students in my cohort at Makers Academy"
+	puts "-------------------------------------------"
+end
+
+def print (students)
+	students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	end
+end
+
+def print_footer(names)
+	puts "---------------------------------"
+	puts "Overall, we have #{names.length} in our cohort."
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
