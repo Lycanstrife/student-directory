ba051125b0f8b94d69f3eecb597cc1e1075baf24
Matt Thomas <matt@platformprojects.org>
2014-02-24 14:07:53 +0000

    Minor refactoring, including declaring the students methos OUTSIDE of the methods

--- directory_pro.rb
+++ directory_pro.rb
@@ -1,42 +1,50 @@
+@students = []
+
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+end
+
 def interactive_menu
-	students = []
+	@students = []
 	loop do
-	# 1. Print the menu and ask the user what to do
-	  puts "1. Input the students"
-	  puts "2. Show the students"
-	  puts "9. Exit"
-	# 2. Read the input and save it into a variable
 	  selection = gets.chomp
-	# 3. Do what the user has asked
 	  case selection
 	  	when "1"
 	  		students = input_students
 	  	when "2"
-	  		print_header
-	  		print(students)
-	  		print_footer(students)
+	  		students = show_students
 	  	when "9"
-	  		exit # This will cause the program to terminate
+	  		exit
 	  	else
 	  		puts "I don't know what you meant, try again"
 	  	end
 	end
 end
 
+def show_students
+	print_header
+	print(@students)
+	print_footer(@students)
+	print_menu
+end
+
 def input_students
 	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
-	students = []
+	@students = []
 	name = gets.chomp
 	while !name.empty? do
 		puts "Enter Cohort"
 		cohort = gets.chomp
 		cohort = "N/A" if cohort.empty?
-		students << {:name => name, :cohort => cohort}
-		puts "Now we have #{students.length} students. Any more?"
+		@students << {:name => name, :cohort => cohort}
+		puts "Now we have #{@students.length} students. Any more?"
 		name = gets.chomp
 	end
-	students
+	@students
+	print_menu
 end
 
 def print_header
@@ -55,7 +63,8 @@ def print_footer(names)
 	puts "Overall, we have #{names.length} in our cohort."
 end
 
-students = interactive_menu
+print_menu
+print(interactive_menu)
 print_header
-print(students)
-print_footer(students)
+print(@students)
+print_footer(@students)
